ad9ed80d7d2454fb4bedec2cfa7a5880ac08843c
Reubems <95reuben@gmail.com>
2018-04-06 17:51:02 +0200

    Created Customer API

---
+++ CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CustomerApi.Models;
+using CustomerApi.Data;
+
+namespace CustomerApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Customer")]
+    public class CustomerController : Controller
+    {
+
+        private readonly IRepository<Customer> repository;
+
+        public CustomerController(IRepository<Customer> repos)
+        {
+            repository = repos;
+        }
+
+        // GET: api/customers
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return repository.GetAll();
+        }
+
+        // GET api/customers/5
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public IActionResult Get(int id)
+        {
+            var item = repository.Get(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // POST api/customers
+        [HttpPost]
+        public IActionResult Post([FromBody]Customer customer)
+        {
+            if (customer == null)
+            {
+                return BadRequest();
+            }
+
+            var newCustomer = repository.Add(customer);
+
+            return CreatedAtRoute("GetCustomer", new { id = newCustomer.Id }, newCustomer);
+        }
+
+        // PUT api/customers/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Customer customer)
+        {
+            if (customer == null || customer.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var modifiedCustomer = repository.Get(id);
+
+            if (modifiedCustomer == null)
+            {
+                return NotFound();
+            }
+
+            modifiedCustomer.Email = customer.Email;
+            modifiedCustomer.Phone = customer.Phone;
+            modifiedCustomer.BillingAddress = customer.BillingAddress;
+            modifiedCustomer.ShippingAddress = customer.ShippingAddress;
+
+            repository.Edit(modifiedCustomer);
+            return new NoContentResult();
+        }
+
+        // DELETE api/customers/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (repository.Get(id) == null)
+            {
+                return NotFound();
+            }
+
+            repository.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
---
+++ CustomerApi/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CustomerApi.Models;
+using System;
+
+namespace CustomerApi.Data
+{
+    public static class DbInitializer
+    {
+        // This method will create and seed the database.
+        public static void Initialize(CustomerApiContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            // Look for any Products
+            if (context.Customers.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { Id=1, BillingAddress = "Havnegade 63 B,  6700, Esbjerg", CompanyName = "EASV", Email = "trialemail@easv.dk", Phone ="1345 5684", ShippingAddress = "Havnegade 63 B,  6700, Esbjerg" }
+            };
+
+
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+        }
+    }
+}
